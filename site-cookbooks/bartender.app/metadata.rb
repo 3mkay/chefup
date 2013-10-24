36bb26cd0d6f4779dfff6e36a073923d0ac803ac
Mike King <mike@gir.pm>
2013-10-24 22:43:19 +0100

    Copied and edited file from gitx to bartender

---
+++ site-cookbooks/bartender.app/metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "Mike King"
+maintainer_email  "hello@gir.pm"
+license           "MIT"
+description       "Install Bartender"
+version           "1.0.0"
+supports          "mac_os_x"
