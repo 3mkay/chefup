0d909c2d48ebd31de9e2d377054851e63c22e84d
Mike King <mike@gir.pm>
2013-10-24 22:45:50 +0100

    Another file removal

--- site-cookbooks/heroku-toolbelt/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer        "Mynewsdesk"
-maintainer_email  "dev@mynewsdesk.com"
-license           "MIT"
-description       "Install Heroku Toolbelt"
-version           "1.0.0"
-supports          "mac_os_x"
-depends           "homebrew"
