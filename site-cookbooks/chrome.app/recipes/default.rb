dba3c81d4f6fe2af72b006d57122a7796a42faae
Mike King <mike@gir.pm>
2013-10-25 09:23:43 +0100

    Added a check to see if the Chrome already exists in the Applications folder.

--- site-cookbooks/chrome.app/recipes/default.rb
+++ site-cookbooks/chrome.app/recipes/default.rb
@@ -1,11 +1,15 @@
-directory "/Applications" do
-  mode 00755
-end
+if File.exists?("/Applications/Google Chrome.app")
+  Chef::Log.info "Already installed; to upgrade, remove /Applications/Google Chrome.app"
+else
+  directory "/Applications" do
+    mode 00755
+  end
 
-dmg_package "Google Chrome" do
-  dmg_name "googlechrome"
-  source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
-  destination "/Applications"
-  action :install
+  dmg_package "Google Chrome" do
+    dmg_name "googlechrome"
+    source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
+    destination "/Applications"
+    action :install
+  end
 end
 
