0a204d954f84abd07db9cfc61aa91b81f9f237db
Mike King <mike@gir.pm>
2013-10-25 11:54:57 +0100

    Changed the DMG installer to zip_app installer and added dependency into metadata file

--- site-cookbooks/carboncopycloner/recipes/default.rb
+++ site-cookbooks/carboncopycloner/recipes/default.rb
@@ -1,6 +1,3 @@
-dmg_package "carboncopycloner" do
+zip_app_package "carboncopycloner" do
   source "http://files.bombich.com/ccc-3.5.3.zip"
-  type "zip"
-  action :install
-  owner node['current_user']
 end
