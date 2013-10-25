626dd9ed7b8aae9d199a215f068b15c2fbdb6053
Mike King <mike@gir.pm>
2013-10-25 09:56:56 +0100

    Added a drush book

---
+++ site-cookbooks/drush/metadata.rb
@@ -0,0 +1,8 @@
+name             "tig"
+maintainer       "Mike King"
+maintainer_email "mike@gir.pm"
+license          "Apache 2.0"
+description      "Install tig from homebrew"
+version          "1.0.0"
+supports         "mac_os_x"
+depends          "homebrew"
