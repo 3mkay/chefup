613c11fe9137489f3fc475564c8cac24be9c4798
Mike King <mike@gir.pm>
2013-10-25 13:41:45 +0100

    Added 'accept_eula true' for Opera install

--- site-cookbooks/opera/recipes/default.rb
+++ site-cookbooks/opera/recipes/default.rb
@@ -2,4 +2,5 @@ dmg_package "Opera" do
   source "http://get.geo.opera.com/pub/opera/desktop/17.0.1241.53/mac/Opera_17.0.1241.53_Setup.dmg"
   action :install
   owner node['current_user']
+  accept_eula true
 end
