501f0354c2435ca23b07c7498d325aa3cedd3a7f
Mike King <mike@gir.pm>
2013-10-25 11:21:05 +0100

    Updated libreoffice download

--- site-cookbooks/libreoffice/recipes/default.rb
+++ site-cookbooks/libreoffice/recipes/default.rb
@@ -1,6 +1,6 @@
 dmg_package "LibreOffice" do
-  volumes_dir dmg_properties['volumes_dir']
-  source dmg_properties['source']
-  checksum dmg_properties['checksum']
+  source "https://donate.libreoffice.org/home/dl/mac-x86/4.1.2/en-US/LibreOffice_4.1.2_MacOS_x86.dmg"
+  type 'dmg'
+  action :install
   owner node['current_user']
 end
