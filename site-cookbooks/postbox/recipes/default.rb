6b9cf10a05ac1f7c916585ee5562de07a260d9b8
Mike King <mike@gir.pm>
2013-10-25 13:58:01 +0100

    Added eula accept to Postbox book

--- site-cookbooks/postbox/recipes/default.rb
+++ site-cookbooks/postbox/recipes/default.rb
@@ -2,4 +2,5 @@ dmg_package "Postbox" do
   source "http://download.getpostbox.com.s3.amazonaws.com/installers/3.0.8/2_8d9571a45ed7132c438b4c02e678dbd1838a7be5/postbox-3.0.8-mac64.dmg"
   action :install
   owner node['current_user']
+  accept_eula true
 end
