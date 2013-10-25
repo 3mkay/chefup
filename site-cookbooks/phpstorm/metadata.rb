100fae558ae4a0f2fe3a834d2c2971bf2da37ed5
Mike King <mike@gir.pm>
2013-10-25 12:34:04 +0100

    Added PhpStorm

---
+++ site-cookbooks/phpstorm/metadata.rb
@@ -0,0 +1,8 @@
+name             "PhpStorm"
+maintainer       "Mike King"
+maintainer_email "mike@gir.pm"
+license          "Apache 2.0"
+description      "Install PhpStorm"
+version          "1.0.0"
+supports         "mac_os_x"
+depends          "dmg"
