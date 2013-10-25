414657f2b21c3f5a389d8b3734d411e49af94a9a
Mike King <mike@gir.pm>
2013-10-25 10:27:43 +0100

    Deleted Sequel Pro file and made a cookbook

---
+++ site-cookbooks/sequel_pro/recipes/default.rb
@@ -0,0 +1,6 @@
+dmg_package "Sequel Pro" do
+  volumes_dir "Sequel Pro 1.0"
+  owner node['current_user']
+  source "https://sequel-pro.googlecode.com/files/sequel-pro-1.0.2.dmg"
+  action :install
+end
