1f605b0683e2f704a579f23711e27a58a7d58838
Mike King <mike@gir.pm>
2013-10-25 12:11:41 +0100

    Updated vagrant to install the pkg from inside the dmg

--- site-cookbooks/vagrant/recipes/default.rb
+++ site-cookbooks/vagrant/recipes/default.rb
@@ -1,6 +1,6 @@
 dmg_package "Vagrant" do
   source "http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg"
-  type 'dmg'
+  type 'pkg'
   action :install
   owner node['current_user']
 end
