ab8e03f2cac1a1ca7ec07b5ff2d53aa7bbc3ce61
Mike King <mike@gir.pm>
2013-10-25 10:47:54 +0100

    Updated VLC recipe to point to the latest version

--- site-cookbooks/vlc/recipes/default.rb
+++ site-cookbooks/vlc/recipes/default.rb
@@ -1,7 +1,5 @@
 dmg_package "VLC" do
-  dmg_name "vlc-#{vlc_version}"
-  volumes_dir "vlc-#{vlc_version}"
-  source "http://download.videolan.org/vlc/#{vlc_version}/macosx/vlc-#{vlc_version}.dmg"
+  source "http://mirrors.neterra.net/vlc/vlc/2.1.0/macosx/vlc-2.1.0.dmg"
   checksum vlc_checksum
   action :install
   owner node['current_user']
