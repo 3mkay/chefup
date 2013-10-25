e5180ff868d66959ce6a37972c046c1b4f7b5c6b
Mike King <mike@gir.pm>
2013-10-25 11:25:03 +0100

    Updated virtualbox download

--- site-cookbooks/vagrant/recipes/default.rb
+++ site-cookbooks/vagrant/recipes/default.rb
@@ -1,8 +1,6 @@
 dmg_package "Vagrant" do
-  source   dmg_properties['source']
-  checksum dmg_properties['checksum']
+  source "http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg"
+  type 'dmg'
   action :install
-  type "pkg"
   owner node['current_user']
-  package_id "com.vagrant.vagrant"
 end
