4b7d2fc0fd342d777c2d1a2347edbec303a0a393
Mike King <emkay@Mikes-MacBook-Pro.local>
2013-10-27 20:44:58 +0000

    Alfred recipe updated so that it no longer tries to use the [user]/Application folder

--- site-cookbooks/alfred.app/recipes/default.rb
+++ site-cookbooks/alfred.app/recipes/default.rb
@@ -1,13 +1,13 @@
 if File.exists?("/Applications/Alfred 2.app")
   Chef::Log.info "Already installed; to upgrade, remove /Applications/Alfred 2.app"
 else
-  directory "#{ENV['HOME']}/Applications" do
+  directory "/Applications" do
     mode 00755
   end
 
   dmg_package "Alfred" do
     volumes_dir "Alfred.app"
-    destination "#{ENV['HOME']}/Applications"
+    destination "/Applications"
     source "http://cachefly.alfredapp.com/Alfred_2.0.9_214.zip"
     action :install
   end
