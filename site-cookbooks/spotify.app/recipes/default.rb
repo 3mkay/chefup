fa33fad93d26976fecb62b2060324e91894a58d5
Mike King <mike@gir.pm>
2013-10-30 08:46:09 +0000

    Added some checks to the spotify cookbook to make sure that it doesn't do stuff if the file is already in cache or has already been uncompressed.

--- site-cookbooks/spotify.app/recipes/default.rb
+++ site-cookbooks/spotify.app/recipes/default.rb
@@ -1,12 +1,17 @@
 if File.exists?("/Applications/Spotify.app")
   Chef::Log.info "Already installed; to upgrade, remove /Applications/Spotify.app"
 else
-  remote_file "#{Chef::Config[:file_cache_path]}/SpotifyInstaller.zip" do
-    source "http://download.spotify.com/SpotifyInstaller.zip"
-  end
 
-  execute "unzip SpotifyInstaller" do
-    command "unzip #{Chef::Config[:file_cache_path]}/SpotifyInstaller.zip -d #{Chef::Config[:file_cache_path]}/"
+  unless File.exists?("#{Chef::Config[:file_cache_path]}/SpotifyInstaller.zip")
+    remote_file "#{Chef::Config[:file_cache_path]}/SpotifyInstaller.zip" do
+      source "http://download.spotify.com/SpotifyInstaller.zip"
+    end
+  end
+  
+  unless File.exists?("#{Chef::Config[:file_cache_path]}/Install Spotify.app")
+    execute "unzip SpotifyInstaller" do
+      command "unzip #{Chef::Config[:file_cache_path]}/SpotifyInstaller.zip -d #{Chef::Config[:file_cache_path]}/"
+    end
   end
 
   execute "Run SpotifyInstaller" do
